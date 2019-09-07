8edc33a0d6039daa27ff108d3c54244e591838da
Christopher Maree <christopher.maree@gmail.com>
2019-09-07 16:11:13 +0200

    Fixed Timeline

--- sections/Home/Timeline/index.vue
+++ sections/Home/Timeline/index.vue
@@ -14,11 +14,9 @@
             <div class="col-2 text-left bottom">
               <div class="circle">1</div>
             </div>
-            <div class="col-6" style="padding-left:0px;">
-              <p class="h4">2015-2018</p>
-              <p>
-                Concept research and development as a non profit adherence initiative, in collaboration with SA Department of Health, Mylan Pharmaceuticals and a prominent health data analytics firm.
-              </p>
+            <div class="col-10" style="padding-left:0px;">
+              <p class="h2">2015 to 2017</p>
+              <p>Concept research and development as a non profit adherence initiative, in collaboration with SA Department of Health, Mylan Pharmaceuticals and a prominent health data analytics firm.</p>
             </div>
           </div>
           <div class="row timeline">
@@ -33,11 +31,28 @@
             </div>
           </div>
           <div class="row align-items-center justify-content-end how-it-works">
-            <div class="col-6 text-right" style="padding-right:0px;">
-              <p class="h4">2018</p>
-              <p class="h6">Q1</p>
+            <div class="col-5" style="padding-right:0px;">
+              <p class="h2">2018</p>
+              <p class="h5">Q1</p>
+              <p>Ribbon Blockchain incorporated as a privately limited for profit company with a public health blockchain use case.</p>
+
+              <p class="h5">Q3</p>
               <p>
-                Ribbon Blockchain incorporated as a privately limited for profit company with a public health blockchain use case.
+                Private sector & Big Pharmaceuticals engagement with MOUs.
+                White paper development.
+              </p>
+            </div>
+
+            <div class="col-5" style="padding-right:0px;">
+              <p class="h4"></p>
+              <p class="h5">Q2</p>
+              <p>Development of Incentives demo app and website, and health public sector stakeholder engagements.</p>
+
+              <p class="h5">Q4</p>
+              <p>
+                Incentives App MVP launch.
+                Stakeholder exhibit workshops.
+                South Africa National Blockchain Alliance Board membership.
               </p>
             </div>
             <div class="col-2 text-center full">
@@ -55,15 +70,38 @@
               <div class="corner top-left"></div>
             </div>
           </div>
-          <div class="row align-items-center how-it-works">
-            <div class="col-2 text-center full p-lg-2" style="margin-left:3px">
+          <div class="row how-it-works" style="padding-left:3px">
+            <div class="col-2 text-center full p-lg-2">
               <div class="circle">3</div>
             </div>
-            <div class="col-6" style="padding-left:0px;">
-              <p class="h4">2018</p>
-              <p class="h6">Q2</p>
+
+            <div class="col-5">
+              <p class="h2">2019</p>
+              <p class="h5">Q1</p>
+              <p>Initial incentives POC.</p>
+
+              <p class="h5">Q3</p>
               <p>
-                Development of Incentives demo app and website, and health public sector stakeholder engagements
+                App Iteration.
+                Onboarding patients and practitioners.
+                Incentives Design.
+                Partnership with R2C.
+              </p>
+            </div>
+
+            <div class="col-5">
+              <p class="h4"></p>
+              <p class="h5">Q2</p>
+              <p>
+                Proof Of concept Token design.
+                Partnership engagement.
+                Consensys Social Impart Grant.
+              </p>
+
+              <p class="h5">Q4</p>
+              <p>
+                Dai-to-Fiat cash off-ramps.
+                International Fiat Donation portal.
               </p>
             </div>
           </div>
@@ -78,212 +116,37 @@
               <div class="corner left-bottom"></div>
             </div>
           </div>
-          <div class="row align-items-center justify-content-end how-it-works">
-            <div class="col-6 text-right" style="padding-right:0px;">
-              <p class="h4">2018</p>
-              <p class="h6">Q3</p>
+          <div class="row align-items-center justify-content-end how-it-works" style="padding-right:3px">
+            <div class="col-5">
+              <p class="h2">2020</p>
+              <p class="h5">Q1</p>
+              <p>USSD token wallets.
+                DAO program managers</p>
+
+              <p class="h5">Q3</p>
               <p>
-                Private sector & Big Pharmaceuticals engagement with MOUs
-              </p>
-              <p>
-                White paper development
+                xDAI integration.
+                Wallet Point of Sale and Inventory management system.
               </p>
             </div>
-            <div class="col-2 text-center full">
+
+            <div class="col-5">
+              <p class="h4"></p>
+              <p class="h5">Q2</p>
+              <p>
+                Extra system functionality, Patient Queuing, push notifications More advanced token modeling.
+              </p>
+
+              <p class="h5">Q4</p>
+              <p>
+                Real-time Data Analytics.
+                Begin planning creation of data marketplace.
+              </p>
+            </div>
+            <div class="col-2 text-center top">
               <div class="circle">4</div>
             </div>
           </div>
-          <div class="row timeline">
-            <div class="col-2">
-              <div class="corner right-bottom"></div>
-            </div>
-            <div class="col-8">
-              <hr />
-            </div>
-            <div class="col-2">
-              <div class="corner top-left"></div>
-            </div>
-          </div>
-          <div class="row align-items-center how-it-works">
-            <div class="col-2 text-center full p-lg-2" style="margin-left:3px">
-              <div class="circle">5</div>
-            </div>
-            <div class="col-6" style="padding-left:0px;">
-              <p class="h4">2018</p>
-              <p class="h6">Q4</p>
-              <p>
-                Incentives App MVP launch
-              </p>
-              <p>
-                Stakeholder exhibit workshops
-              </p>
-              <p>
-                South Africa National Blockchain Alliance Board membership
-              </p>
-            </div>
-          </div>
-          <div class="row timeline">
-            <div class="col-2">
-              <div class="corner top-right"></div>
-            </div>
-            <div class="col-8">
-              <hr />
-            </div>
-            <div class="col-2">
-              <div class="corner left-bottom"></div>
-            </div>
-          </div>
-          <div class="row align-items-center justify-content-end how-it-works">
-            <div class="col-6 text-right" style="padding-right:0px;">
-              <p class="h4">2019</p>
-              <p class="h6">Q1 - Q2</p>
-              <p>
-               Initial incentives system proof of concept
-              </p>
-            </div>
-            <div class="col-2 text-center top" style="margin-right:3px">
-              <div class="circle">6</div>
-            </div>
-          </div>
-          <div class="row timeline">
-            <div class="col-2">
-              <div class="corner right-bottom"></div>
-            </div>
-            <div class="col-8">
-              <hr />
-            </div>
-            <div class="col-2">
-              <div class="corner top-left"></div>
-            </div>
-          </div>
-          <div class="row align-items-center how-it-works">
-            <div class="col-2 text-center full p-lg-2" style="margin-left:3px">
-              <div class="circle">7</div>
-            </div>
-            <div class="col-6" style="padding-left:0px;">
-              <p class="h4">2019</p>
-              <p class="h6">Q3</p>
-              <p>
-                App Iteration, Onboarding patients and practitioners into web3, Incentives Design
-              </p>
-            </div>
-          </div>
-          <div class="row timeline">
-            <div class="col-2">
-              <div class="corner top-right"></div>
-            </div>
-            <div class="col-8">
-              <hr />
-            </div>
-            <div class="col-2">
-              <div class="corner left-bottom"></div>
-            </div>
-          </div>
-          <div class="row align-items-center justify-content-end how-it-works">
-            <div class="col-6 text-right" style="padding-right:0px;">
-              <p class="h4">2019</p>
-              <p class="h6">Q4</p>
-              <p>
-                Dai-to-Fiat cash off-ramps and donation portal
-              </p>
-            </div>
-            <div class="col-2 text-center full">
-              <div class="circle">8</div>
-            </div>
-          </div>
-          <div class="row timeline">
-            <div class="col-2">
-              <div class="corner right-bottom"></div>
-            </div>
-            <div class="col-8">
-              <hr />
-            </div>
-            <div class="col-2">
-              <div class="corner top-left"></div>
-            </div>
-          </div>
-          <div class="row align-items-center how-it-works">
-            <div class="col-2 text-center full p-lg-2" style="margin-left:3px">
-              <div class="circle">9</div>
-            </div>
-            <div class="col-6" style="padding-left:0px;">
-              <p class="h4">2020</p>
-              <p class="h6">Q1</p>
-              <p>
-                USSD token wallets and DAO program managers  
-              </p>
-            </div>
-          </div>
-          <div class="row timeline">
-            <div class="col-2">
-              <div class="corner top-right"></div>
-            </div>
-            <div class="col-8">
-              <hr />
-            </div>
-            <div class="col-2">
-              <div class="corner left-bottom"></div>
-            </div>
-          </div>
-          <div class="row align-items-center justify-content-end how-it-works">
-            <div class="col-6 text-right" style="padding-right:0px;">
-              <p class="h4">2020</p>
-              <p class="h6">Q2</p>
-              <p>
-                Extra system functionality, Patient Queuing, push notifications and more
-              </p>
-            </div>
-            <div class="col-2 text-center full">
-              <div class="circle">10</div>
-            </div>
-          </div>
-          <div class="row timeline">
-            <div class="col-2">
-              <div class="corner right-bottom"></div>
-            </div>
-            <div class="col-8">
-              <hr />
-            </div>
-            <div class="col-2">
-              <div class="corner top-left"></div>
-            </div>
-          </div>
-           <div class="row align-items-center how-it-works">
-            <div class="col-2 text-center full p-lg-2" style="margin-left:3px">
-              <div class="circle">11</div>
-            </div>
-            <div class="col-6" style="padding-left:0px;">
-              <p class="h4">2020</p>
-              <p class="h6">Q3</p>
-              <p>
-                xDAI wallet Point of Sale and Inventory management system integration 
-              </p>
-            </div>
-          </div>
-          <div class="row timeline">
-            <div class="col-2">
-              <div class="corner top-right"></div>
-            </div>
-            <div class="col-8">
-              <hr />
-            </div>
-            <div class="col-2">
-              <div class="corner left-bottom"></div>
-            </div>
-          </div>
-          <div class="row align-items-center justify-content-end how-it-works">
-            <div class="col-6 text-right" style="padding-right:0px;">
-              <p class="h4">2020</p>
-              <p class="h6">Q4</p>
-              <p>
-               Real-time Data Analytics 
-              </p>
-            </div>
-            <div class="col-2 text-center top" style="margin-right:3px">
-              <div class="circle">6</div>
-            </div>
-          </div>
-
         </div>
       </div>
     </div>
@@ -292,3 +155,4 @@
 <script>
 export default {};
 </script>
+
