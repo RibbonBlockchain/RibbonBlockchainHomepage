c6693e5fc5c6f88df6a711b721654a4dab642b84
Christopher Maree <christopher.maree@gmail.com>
2019-09-07 14:55:07 +0200

    Refactored how it works

--- sections/Home/UserFlow/index.vue
+++ sections/Home/UserFlow/index.vue
@@ -7,16 +7,16 @@
           <div class="col-md-12">
             <div class="service_title title text-center">
               <h3>How Ribbon works</h3>
-              <p>The interactions with ribbon can be broken down into 4 steps</p>
+              <p>The Ribbon ecosystem has 4 main features to bring together donors, patients, practitioners and governements.</p>
             </div>
           </div>
           <div class="w-100 my-4" id="crowdfunding"></div>
-          <div class="col-md-5 offset-md-1" >
+          <div class="col-md-5 offset-md-1">
             <img src="img/graphics/CrowdFunding.png" class="inner" style="max-height:350px" alt />
           </div>
           <div class="col-md-5 my-auto">
             <div class="smaple_text">
-              <h3>1 - Crowdfunded Incentives</h3>
+              <h3>Crowdfunded Incentives</h3>
               <h5 style="padding-bottom:0px; padding-top: 15px;">
                 We tokenize crowdfunded healthcare incentives
                 from:
@@ -48,7 +48,7 @@
           </div>
           <div class="col-md-5 my-auto">
             <div class="smaple_text">
-              <h3>2 - Patient Incentives</h3>
+              <h3>Patient Incentives</h3>
               <h5
                 style="padding-bottom:0px; padding-top: 15px;"
               >We channel Incentivizes to Patients for:</h5>
@@ -87,7 +87,7 @@
           </div>
           <div class="col-md-5 my-auto">
             <div class="smaple_text">
-              <h3>3 - Practitioner Incentives</h3>
+              <h3>Practitioner Incentives</h3>
               <h5 style="padding-bottom:0px; padding-top: 15px;">
                 We channel Incentivizes to healthcare
                 practitioners for:
@@ -113,7 +113,7 @@
           </div>
           <div class="col-md-5 my-auto">
             <div class="smaple_text">
-              <h3>4 - Analytics</h3>
+              <h3>Analytics</h3>
               <h5 style="padding-bottom:0px; padding-top: 15px;">We generate analytics reports on:</h5>
               <ul style="padding-top:0px">
                 <li>
@@ -136,39 +136,6 @@
           </div>
           <div class="col-md-5 offset-md-1">
             <img src="img/graphics/Analytics.png" class="inner" style="max-height:350px" alt />
-          </div>
-          <div class="w-100 midle_shape_1 text-center"  id="technology">
-            <img src="img/case/single/shape2.png" alt />
-          </div>
-          <div class="col-md-5 offset-md-1">
-            <img src="img/graphics/tech.png" class="inner" style="max-height:350px" alt />
-          </div>
-          <div class="col-md-5 my-auto">
-            <div class="smaple_text">
-              <h3>5 - Technology</h3>
-              <h5 style="padding-bottom:0px; padding-top: 15px;">
-                Our technology includes the following:
-              </h5>
-              <h5>USSD</h5>
-              <ul style="padding-top:0px">
-                <li>
-                  <i class="fas fa-caret-right"></i> The USSD payment system will allow tokens to be sent and received. This technology allows any user with a connection to a mobile telecommunications tower to have access to the blockchain. The system uses a USSD gateway to connect to our backend API to allow for transactions to be made. This all works without an internet connection and on any mobile phone with a working sim card. 
-
-                </li>
-                <h5>DAO</h5>
-                <li>
-                  <i class="fas fa-caret-right"></i> The DAO allows for transparency of funds from a donor or sponsor to the end user. That entire process is recorded and information is sent back to the donor. 
-
-                </li>
-                <li>
-                  <i class="fas fa-caret-right"></i> The funds are first crowdfunded by a program, all of those funds go directly into the DAO and only get sent out to the people it's supposed to. Everything is tracked and if there's any malicious intent then a user will know and they can then report that program. The funds will be returned and the program will be deleted. 
-                </li>
-                <h5>DAI</h5>
-                <li>
-                  <i class="fas fa-caret-right"></i> The system will use a stable coin called DAI. Dai is pegged to the US dollar and is significantly more stable than crypto-currencies as well as local African currencies. This makes it easier to transact with as there is a set dollar amount people can earn every month from Ribbon tasks. 
-                </li>
-              </ul>
-            </div>
           </div>
         </div>
       </div>
