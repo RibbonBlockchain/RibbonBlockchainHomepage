faafdcc6edaa9ae61a7184b29f1e3f30aa055c70
yemisi567 <alegbeyemi@domain.com>
2021-02-19 19:19:12 -0800

    Edit homepage

--- sections/Home/Team/index.vue
+++ sections/Home/Team/index.vue
@@ -6,31 +6,41 @@
           <div class="title text-center">
             <h3>Core team</h3>
             <p>
-              Ribbon has a experienced, interdisciplinary team who is perfectly positioned to realise the
-              potential of healthcare incentives.
+              Ribbon has a experienced, interdisciplinary team who is perfectly
+              positioned to realise the potential of healthcare incentives.
             </p>
           </div>
         </div>
         <div class="w-100"></div>
         <div class="col-md-3">
-          <div class="teams_single_in" data-aos="fade-up" data-aos-duration="1000">
-            <img src="/img/team/gugu.jpg" alt="Ribbon Blockchain" />
+          <div
+            class="teams_single_in"
+            data-aos="fade-up"
+            data-aos-duration="1000"
+          >
+            <img src="/img/team/mpadi.jpg" alt="Ribbon Blockchain" />
             <ul class="social">
               <li>
-                <a href="https://www.linkedin.com/in/gugu-newman-nyathi-11ab17a7/">
+                <a
+                  href="https://www.linkedin.com/in/gugu-newman-nyathi-11ab17a7/"
+                >
                   <i class="fab fa-linkedin-in"></i>
                 </a>
               </li>
             </ul>
             <div class="name">
-              <h4>Gugulethu Nyathi</h4>
-              <p class="h6" style="padding-top: 5px">Co Founder and COO</p>
+              <h4>Mpadi Makgalo</h4>
+              <p class="h6" style="padding-top: 5px">Co Founder and CEO</p>
             </div>
           </div>
         </div>
         <div class="col-md-3">
-          <div class="teams_single_in" data-aos="fade-up" data-aos-duration="1000">
-            <img src="/img/team/chris.jpg" alt="Ribbon Blockchain" />
+          <div
+            class="teams_single_in"
+            data-aos="fade-up"
+            data-aos-duration="1000"
+          >
+            <img src="/img/team/BenOnuoha.jpeg" alt="Ribbon Blockchain" />
             <ul class="social">
               <li>
                 <a href="https://github.com/chrismaree">
@@ -44,14 +54,18 @@
               </li>
             </ul>
             <div class="name">
-              <h4>Chris Maree</h4>
-              <p class="h6" style="padding-top: 5px">Co Founder and CTO</p>
+              <h4>Ben Onuoha</h4>
+              <p class="h6" style="padding-top: 5px">Co-Founder and CSO</p>
             </div>
           </div>
         </div>
         <div class="col-md-3">
-          <div class="teams_single_in" data-aos="fade-up" data-aos-duration="1000">
-            <img src="/img/team/mpadi.jpg" alt="Ribbon Blockchain" />
+          <div
+            class="teams_single_in"
+            data-aos="fade-up"
+            data-aos-duration="1000"
+          >
+            <img src="/img/team/gugu.jpg" alt="Ribbon Blockchain" />
             <ul class="social">
               <li>
                 <a href="https://www.linkedin.com/in/busisiwe/">
@@ -60,17 +74,27 @@
               </li>
             </ul>
             <div class="name">
-              <h4>Mpadi Makgalo</h4>
-              <p class="h6" style="padding-top: 5px">Co Founder and CEO</p>
+              <h4>Gugulethu Nyathi</h4>
+              <p class="h6" style="padding-top: 5px">Co Founder and COO</p>
             </div>
           </div>
         </div>
         <div class="col-md-3">
-          <div class="teams_single_in" data-aos="fade-up" data-aos-duration="1000">
-            <img src="/img/team/AllanKatongole.jpg" alt="Ribbon Blockchain" />
+          <div
+            class="teams_single_in"
+            data-aos="fade-up"
+            data-aos-duration="1000"
+          >
+            <img
+              src="/img/team/bamidele.jpg"
+              style="height: 260px"
+              alt="Ribbon Blockchain"
+            />
             <ul class="social">
               <li>
-                <a href="https://www.linkedin.com/in/katongole-allan-767670118/">
+                <a
+                  href="https://www.linkedin.com/in/katongole-allan-767670118/"
+                >
                   <i class="fab fa-linkedin-in"></i>
                 </a>
               </li>
@@ -86,15 +110,50 @@
               </li>
             </ul>
             <div class="name">
-              <h4>Allan Katongole</h4>
-              <p class="h6" style="padding-top: 5px">Software Engineer</p>
+              <h4>Alegbe Bamidele</h4>
+              <p class="h6" style="padding-top: 5px">CTO</p>
             </div>
           </div>
         </div>
       </div>
       <div class="row">
-        <div class="offset-md-1 col-md-3">
-          <div class="teams_single_in" data-aos="fade-up" data-aos-duration="1000">
+        <div class=" col-md-3">
+          <div
+            class="teams_single_in"
+            data-aos="fade-up"
+            data-aos-duration="1000"
+          >
+            <img src="/img/team/AllanKatongole.jpg" alt="Ribbon Blockchain" />
+            <ul class="social">
+              <li>
+                <a href="https://github.com/iamonuwa">
+                  <i class="fab fa-github"></i>
+                </a>
+              </li>
+              <li>
+                <a href="https://gitlab.com/iamonuwa">
+                  <i class="fab fa-gitlab"></i>
+                </a>
+              </li>
+              <li>
+                <a href="https://www.linkedin.com/in/iamonuwa/">
+                  <i class="fab fa-linkedin-in"></i>
+                </a>
+              </li>
+            </ul>
+            <div class="name">
+              <h4>Allan Katongole</h4>
+              <p class="h6" style="padding-top: 5px">Software Engineer</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-3">
+          <div
+            class="teams_single_in"
+            data-aos="fade-up"
+            data-aos-duration="1000"
+          >
             <img src="/img/team/isaac.jpg" alt="Ribbon Blockchain" />
             <ul class="social">
               <li>
@@ -136,7 +195,11 @@
           </div>
         </div>-->
         <div class="col-md-3">
-          <div class="teams_single_in" data-aos="fade-up" data-aos-duration="1000">
+          <div
+            class="teams_single_in"
+            data-aos="fade-up"
+            data-aos-duration="1000"
+          >
             <img src="/img/team/veronica.jpg" alt />
             <ul class="social">
               <li>
@@ -150,7 +213,9 @@
                 </a>
               </li>
               <li>
-                <a href="https://www.linkedin.com/in/veronica-coutts-aa5915149/">
+                <a
+                  href="https://www.linkedin.com/in/veronica-coutts-aa5915149/"
+                >
                   <i class="fab fa-linkedin-in"></i>
                 </a>
               </li>
@@ -162,7 +227,11 @@
           </div>
         </div>
         <div class="col-md-3">
-          <div class="teams_single_in" data-aos="fade-up" data-aos-duration="1000">
+          <div
+            class="teams_single_in"
+            data-aos="fade-up"
+            data-aos-duration="1000"
+          >
             <img src="/img/team/neo.jpeg" alt />
             <ul class="social">
               <li>
